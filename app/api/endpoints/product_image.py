4452f102dcf996175a9cf0a9053fea8796c84435
darpankattel <kattelktl@gmail.com>
2025-03-08 01:07:16 +0545

    added product image

--- app/api/endpoints/product_image.py
+++ app/api/endpoints/product_image.py
@@ -1,12 +1,15 @@
 from fastapi import APIRouter, Depends
 from app.core.auth import verify_token
 from app.models.stable_diffusion import stable_diffusion_model
+from datetime import datetime
 
 router = APIRouter()
 
 
-@router.post("/product-image")
-async def generate_product_image(prompt: str, token_data: dict = Depends(verify_token)):
+@router.get("/product-image")
+# , token_data: dict = Depends(verify_token)
+async def generate_product_image(prompt: str):
     """Generates product image using Stable Diffusion"""
-    image_path = stable_diffusion_model.generate(prompt)
+    image_path = stable_diffusion_model.generate(
+        prompt, "product", datetime.now().strftime("%Y%m%d%H%M%S"))
     return {"image_url": image_path}
